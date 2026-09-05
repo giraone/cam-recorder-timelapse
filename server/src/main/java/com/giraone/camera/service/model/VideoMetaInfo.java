f894acce0e4875439e4c2b0c4c0c6a70975969cb
Hans Schuell <hans.schuell@gmx.de>
2026-09-05 21:09:06 +0200

    VideoMetaInfo.toString update

--- server/src/main/java/com/giraone/camera/service/model/VideoMetaInfo.java
+++ server/src/main/java/com/giraone/camera/service/model/VideoMetaInfo.java
@@ -3,6 +3,10 @@ package com.giraone.camera.service.model;
 public record VideoMetaInfo(String videoCodec, String audioCodec, int durationSeconds, String resolution, int framesPerSecond) {
     @Override
     public String toString() {
-        return videoCodec + "/" + audioCodec + ", " + resolution + ", " + durationSeconds + "s, " + framesPerSecond + " fps";
+        if (audioCodec != null && !audioCodec.isBlank()) {
+            return videoCodec + "/" + audioCodec + ", " + resolution + ", " + durationSeconds + "s, " + framesPerSecond + " fps";
+        } else {
+            return videoCodec + ", " + resolution + ", " + durationSeconds + "s, " + framesPerSecond + " fps";
+        }
     }
 }
