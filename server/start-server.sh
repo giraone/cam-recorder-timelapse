69a0b829d68ff4653fc5ce8d3dab80e02bfbfede
Hans Schuell <hans.schuell@gmx.de>
2026-09-05 21:42:30 +0200

    Typo fixed

--- server/start-server.sh
+++ server/start-server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -DLOGGING_LEVEL_ROOT=WARN -DLOGGING_LEVEL_COM_GIRAONE_STREAMING=WARN -jar target/cam-recorder.jar
+java -DLOGGING_LEVEL_ROOT=WARN -DLOGGING_LEVEL_COM_GIRAONE_CAMERA=WARN -jar target/cam-recorder.jar
